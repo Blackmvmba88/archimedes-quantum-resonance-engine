ff91b3ef5b1de55be71bcaeaf4230dce86678ec6
BlackMamba <neocyber1@gmail.com>
2026-01-27 17:26:42 -0600

    Create core/__init__.py with core module content

---
+++ core/__init__.py
@@ -0,0 +1,13 @@
+"""
+Archimedes Quantum Resonance Engine - Core Module
+
+Quantum simulation engine for electromagnetic resonance phenomena.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Blackmvmba88"
+
+from . import constants
+from . import quantum_matter
+
+__all__ = ['constants', 'quantum_matter']
